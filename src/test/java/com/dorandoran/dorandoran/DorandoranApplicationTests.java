c31288fa2403e26b572117f1a89c95597d579506
1Bronze <khdkkhdd@gmail.com>
2023-06-18 21:48:01 +0900

    [feat] 프로젝트 생성

---
+++ src/test/java/com/dorandoran/dorandoran/DorandoranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorandoran.dorandoran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorandoranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
